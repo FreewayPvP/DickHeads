a44d695289e47262489f60818b00933e96eeee66
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2017-12-15 23:22:41 +0100

    Add files via upload

---
+++ commands/fancy/Daddy.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+
+class AuthorCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'daddy',
+            group: 'users',
+            memberName: 'daddy',
+            description: 'Gives a nice message in chat!',
+            throttling: {
+                usages: 100,
+                duration: 1
+            }
+        });
+    }
+
+async run(message, args) {
+    return message.channel.send('Give @FreewayPvP#6468 Rank: **Daddy**');
+}}
+
+module.exports = AuthorCommand;
