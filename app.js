ddf2bcca70d2226ed99767d7c0321a893ac14ed1
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2017-12-17 02:42:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ client.on('ready', () => {
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-	client.user.setGame(`with @FreewayPvP#6468`);
-	client.user.setGame(`with @FreewayPvP#6468`,`https://twitch.tv/thomaspvp5`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
 
 const path = require('path');
 
 client.registry
-  .registerGroups([
-		['cool', 'Cool commands'],
-		['fancy', 'fancy Commands'],
-		['games', 'Game Commands'],
-		['alts', 'alt Commands'],
-		['fun', 'Fun Commands']
-	])
-
-	.registerDefaultGroups()
-	.registerDefaultCommands({ help: false })
-	.registerCommandsIn(path.join(__dirname, 'commands'));
+.registerDefaultTypes()
+.registerGroups([
+		['alts', 'Command Group']
+		['cool', 'Command Group']
+		['fancy', 'Command Group']
+		['games', 'Command Group']
+		['fun', 'Command Group']
+])
+.registerDefaultGroups()
+.registerDefaultCommands({ help: false })
+.registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.login('Mzg4NzA2MDUzNjQ0MzUzNTM4.DQ1Rfw.qCOmFwoh8tw3Cz8OBlJCOm1LQHc');
