6d6ddce53f5d9236063083396d5e6a992b75e7f1
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2017-12-17 02:20:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,24 +6,10 @@ const client = new Commando.Client({
 
 const config = require("./config.json");
 
-client.on('ready', () => {
-  console.log(`Hey, I Just Finished Starting up! You can now use me in Discord!`);
-});
-
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(`on ${client.guilds.size} servers`);
-});
-
-client.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
-});
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
-});
+	client.user.setGame(`with @FreewayPvP#6468`);
+	client.user.setGame(`with @FreewayPvP#6468`,`https://twitch.tv/thomaspvp5`);
 
 const path = require('path');
 
